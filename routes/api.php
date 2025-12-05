d0b1c832e043f80ab049da5956386ae9e680c93b
Afiqur Rahman <arrafi245@gmail.com>
2025-12-05 21:50:35 +0600

    api update

--- routes/api.php
+++ routes/api.php
@@ -6,5 +6,5 @@ use Illuminate\Support\Facades\Route;
 // Public chat endpoints
 Route::post('/chat', [ChatController::class, 'chat'])->name('api.chat');
 Route::get('/chatbot/{chatbot_id}', [ChatController::class, 'getChatbotInfo'])->name('api.chatbot.info');
-
+Route::get('/widget-config/{chatbot_id}', [ChatController::class, 'getChatbotInfo'])->name('api.widget.config'); // Alias for widget
 Route::get('/conversation/{session_id}/messages', [ChatController::class, 'getConversationMessages']);
